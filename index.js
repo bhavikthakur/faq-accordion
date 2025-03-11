86f4aaf64ee6319a36d4e07bc18ef63d624930c4
Bhavik Thakur <thakursbhavik@gmail.com>
2025-03-12 00:45:42 +0530

    Add dynamic height change

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const faqHeaders = document.querySelectorAll(".card__faq-header");
-
+const firstAnswer = document.querySelector(".card__answer");
 faqHeaders.forEach((faqHeader) => {
   faqHeader.addEventListener("click", () => {
     const answer = faqHeader.nextElementSibling;
     const icon = faqHeader.querySelector("img");
 
     answer.classList.toggle("active");
-
-    // Toggle between plus and minus icons
-    if (answer.classList.contains("active")) {
-      icon.src = "assets/images/icon-minus.svg";
-      icon.alt = "A minus icon to collapse the answer";
-    } else {
+    if (answer.style.maxHeight && answer.style.maxHeight !== "0px") {
+      answer.style.maxHeight = "0px";
+      answer.style.opacity = "0";
       icon.src = "assets/images/icon-plus.svg";
-      icon.alt = "A plus icon to expand the answer";
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      answer.style.opacity = "1";
+      icon.src = "assets/images/icon-minus.svg";
     }
   });
 });
